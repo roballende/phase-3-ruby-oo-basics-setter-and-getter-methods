8704861c4912aee41f40c23408325f34a30c2f7a
Robert Allende <allende.rob@gmail.com>
2023-01-10 16:40:39 -0600

    commit tree 95c65bd4e66609f8acfbd7aa23b8bd87c73f5cd5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    
+    def name=(set_name) #setter
+        @name = set_name
+    end
+
+    def name            #getter
+        @name
+    end
+
+    def breed=(breed)   #setter
+        @breed = breed
+    end
+
+    def breed           #getter
+        @breed
+    end
+
 
 end
